87cd578eb24979030dba4c3a532141a64dd30400
ALEX321616 <86130136+ALEX321616@users.noreply.github.com>
2021-12-07 11:50:17 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,9 @@
 import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
-//const searchParams = '?fields=name.official,capital,population,flags.svg,languages';
+//const searchParams = '?fields=name.official,capital,population,flags.svg';
 export function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
+  return fetch(`${BASE_URL}${name}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
